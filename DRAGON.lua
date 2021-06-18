5471639778866f681a4737ebbb35b27502ab267d
MADISON1110 <85648934+MADISON1110@users.noreply.github.com>
2021-06-18 14:03:40 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -1125,7 +1125,7 @@ local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
 end
 
-if text == 'المبرمج هيفين and DevSoFi(msg) then
+if text == 'المبرمج هيفين' and DevSoFi(msg) then
 database:del(bot_id..'Srt:Bot') 
 local Text = [[ 
 ]] 
