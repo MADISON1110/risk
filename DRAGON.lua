8c51a61b5386d1e5b55c8a9347d5cee2539c988d
MADISON1110 <85648934+MADISON1110@users.noreply.github.com>
2021-06-23 20:32:17 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -9586,9 +9586,8 @@ end
 return false
 end
 
-if text == "بوت" then  
-local msg_id = msg.id_/2097152/0.5
-Namebot = (database:get(bot_id..'Name:Bot') or 'الفا')
+if text == "بوت" or text == 'البوت' then
+local Namebot = (database:get(bot_id..'Name:Bot') or 'الفا')
 local DRAGON_Msg = {
 'اسمي  '..Namebot..' يا قلبي 🤤💚',
 'اسمي '..Namebot..' يا روحي🙈❤️',
@@ -9600,23 +9599,17 @@ local DRAGON_Msg = {
 'اسمي الكيوت '..Namebot..' 🌝💘',
 'اسمي  '..Namebot..' ياحياتي🧸♥️',
 'اسمي  '..Namebot..' يوتكه🙈🍑',
-'انا '..Namebot..' إلى عمرو مهاب كابوس الكلاب 🦇',
 }
-local Text = [[
- ]]..DRAGON_Msg[math.random(#DRAGON_Msg)]..[[ 
- 
-]]
 
- us = database:get(id_server..":token_username")
- agwa = database:get(id_server..":SUDO:USERNAME")
- agwa = agwa:gsub("%@", "")
-keyboard = {} 
-keyboard.inline_keyboard = {
-{{text = '  مطور البوت 𖠕 ',url="t.me/"..agwa}},
-{{text = '  اضف البوت الي مجموعتك 𖠕 ',url="t.me/"..us.."?startgroup=start"}},
-}
-https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/'..us..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
-return false
+Namebot = DRAGON_Msg[math.random(#DRAGON_Msg)]
+local function getpro(extra, result, success)
+if result.photos_[0] then
+sendPhoto(msg.chat_id_, msg.id_, 0, 1, nil, result.photos_[0].sizes_[1].photo_.persistent_id_,Namebot, msg.id_, msg.id_, "md")
+else
+send(msg.chat_id_, msg.id_,Namebot, 1, 'md')
+end
+end
+tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = bot_id, offset_ = 0, limit_ = 1 }, getpro, nil)
 end
 
 if text=="اذاعه خاص" and msg.reply_to_message_id_ == 0 and Sudo(msg) then 
